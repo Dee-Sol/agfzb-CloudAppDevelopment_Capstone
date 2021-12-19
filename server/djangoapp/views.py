120a62f07fafaba72f47dd96d6b4f4dd637517cd
rolloverflow <57006611+rolloverflow@users.noreply.github.com>
2021-12-19 18:39:14 +0100

    Added Dealership API Querying Feature

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 # from .restapis import related methods
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -93,7 +94,17 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
+        url = "https://04b084a5.eu-gb.apigw.appdomain.cloud/dealership/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        context['dealerships'] = dealerships
+        # Concat all dealer's short name
+        #dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        # context['dealership_list'] = dealerships
+        #context = {'dealer_names': dealer_names}
         return render(request, 'djangoapp/index.html', context)
+        # return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
